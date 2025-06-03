29f8e07bf61964fd8f49946139837fb27ef8b01b
KaberaSamuel <kaberanshutis@gmail.com>
2025-06-03 21:06:26 +0200

    finishing the feature of counting logs per device

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-
-mkdir -p reports
-
-
 echo "Select log file to analyze:"
 echo "1) Heart Rate (heart_rate.log)"
 echo "2) Temperature (temperature.log)"
@@ -11,24 +6,27 @@ echo "3) Water Usage (water_usage.log)"
 echo -n "Enter choice (1-3): "
 read choice
 
-
-case $choice in
-    1) log_file="active_logs/heart_rate.log"
-       device_type="Heart Rate Monitor"
-       ;;
-    2) log_file="active_logs/temperature.log"
-       device_type="Temperature Sensor"
-       ;;
-    3) log_file="active_logs/water_usage.log"
-       device_type="Water Meter"
-       ;;
-    *) echo "Invalid choice. Please select 1-3."
-       exit 1
-       ;;
-esac
-
-
-if [ ! -f "$log_file" ]; then
-    echo "Error: Log file $log_file not found!"
-    exit 1
+# counting logs per device 
+if [ $choice = 1 ];
+then
+   file_count=$(ls -1 "hospital_data/heart_data_archive" | wc -l)
+   message="There are $file_count logs in heart_data_archive"
+elif [ $choice = 2 ]; 
+then
+   file_count=$(ls -1 "hospital_data/temp_data_archive" | wc -l)
+   message="There are $file_count logs in tem_data_archive"
+elif [ $choice = 3 ]; 
+then
+   file_count=$(ls -1 "hospital_data/water_data_archive" | wc -l)
+   message="There are $file_count logs in water_data_archive"
+else 
+   message="Invalid choice"
 fi
+
+echo $message
+
+
+
+
+
+
